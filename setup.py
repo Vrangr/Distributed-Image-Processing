3bc4ab44ad8772cdfc79e4c96ec5e4b3ec05f9dd
Vrangr <vigneshzambre@gmail.com>
2020-11-24 14:45:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="dis_ip",
     version="1.2.0",
-    author="Vignesh Zambre and Pritam Bhaladhare",
+    author="Vignesh Zambre",
     author_email="vyz4@psu.edu",
     description="A small example package",
     # url="https://github.com/pypa/sampleproject",
